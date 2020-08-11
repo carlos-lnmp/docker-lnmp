e799441b75b199d6c1f86f46f88ba06424579b1e
程志鹏 <984010062@qq.com>
2020-08-11 10:55:49 +0800

    update image php7

--- html/demo.php
+++ html/demo.php
@@ -1,7 +1,11 @@
 <?php 
 
-$str = '2020-07-04 00:31:15';
-$php_date  = getdate($str);
-echo "<pre>";
-print_r($php_date);
-echo "<pre>";
+// $str = '2020-07-04 00:31:15';
+// $php_date  = getdate($str);
+// echo "<pre>";
+// print_r($php_date);
+// echo "<pre>";
+// echo "before 14 days ago:".strtotime('-14 days');
+// echo "<br/>";
+// echo "now:".time();
+var_dump( bcadd(1.10, 2.22,2) );
